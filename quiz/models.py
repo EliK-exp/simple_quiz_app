38048ec262434a7a037dfcb9fd3bbdb821e40c6e
Elyas Karimi <elyaskarimidarmiyan@gmail.com>
2022-03-12 14:45:07 +0330

    Created models.

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=255)
+    # category = models.ManyToManyField(Category)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+# is_multi_answer = models.BooleanField(default=False)
+
+
+class Answer(models.Model):
+    answer_text = models.CharField(max_length=255)
+    is_answer = models.BooleanField(default=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
