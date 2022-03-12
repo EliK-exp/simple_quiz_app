d50d980fdcabedffd6f7fce91cebf6b778561ec4
Elyas Karimi <elyaskarimidarmiyan@gmail.com>
2022-03-12 11:40:34 +0330

    Added custom user model and updated settings.py(added AUTH_USER_MODEL = 'core.AppUser'). Also added apps to installed apps. I should have done this in the previous commit. Also run migrations. Also updated .gitignore file.

--- core/models.py
+++ core/models.py
@@ -3,5 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
-
+class AppUser(AbstractUser):
+    # Email field can be overridden here to be unique.
+    pass
 
