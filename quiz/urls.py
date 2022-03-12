001790c0b247e5fb1c51b56c68321c8a0af2a7d9
Elyas Karimi <elyaskarimidarmiyan@gmail.com>
2022-03-12 15:35:06 +0330

    Added view functions for categories and questions. Maybe I will replace them with generic views later.

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('all_categories', views.all_categories, name='all_categories'),
+    path('all_questions', views.all_questions, name='all_questions'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -5,3 +5,13 @@ from django.shortcuts import render
 def home(request):
     context = {}
     return render(request, 'quiz/home.html', context=context)
+
+
+def all_categories(request):
+    context = {}
+    return render(request, 'quiz/all_categories.html', context=context)
+
+
+def all_questions(request):
+    context = {}
+    return render(request, 'quiz/all_questions.html', context=context)
