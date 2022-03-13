1e3729b76361cb5e9dc48c65ed6eba40f7fe7999
Elyas Karimi <elyaskarimidarmiyan@gmail.com>
2022-03-14 00:25:16 +0330

    Added more functionality and some view functions to the app.

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('all_categories', views.all_categories, name='all_categories'),
-    path('all_questions', views.all_questions, name='all_questions'),
+    path('all_categories/', views.all_categories, name='all_categories'),
+    path('all_questions/', views.all_questions, name='all_questions'),
+    path('all_categories/<int:category_id>/', views.category_detail, name='category_detail'),
+    path('delete_category/<int:category_id>/', views.delete_category, name='delete_category'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Question, Answer, Category
 
 
 # Create your views here.
@@ -8,10 +9,27 @@ def home(request):
 
 
 def all_categories(request):
-    context = {}
+    categories = Category.objects.prefetch_related('question_set').all()
+    context = {'all_categories': categories}
     return render(request, 'quiz/all_categories.html', context=context)
 
 
 def all_questions(request):
-    context = {}
+    questions = Question.objects.select_related('category').prefetch_related('answer_set').all()
+    context = {'all_questions': questions}
     return render(request, 'quiz/all_questions.html', context=context)
+
+
+def category_detail(request, category_id):
+    the_category = Category.objects.filter(id=category_id).get()
+    category_questions = Question.objects.select_related('category').prefetch_related('answer_set').filter(
+        category=the_category)
+    # context = {'the_category': the_category}
+    context = {'the_category': the_category, 'category_questions': category_questions}
+    return render(request, 'quiz/category_detail.html', context=context)
+
+
+def delete_category(request, category_id):
+    the_category = Category.objects.filter(id=category_id).get()
+    the_category.delete()
+    return redirect('all_categories')
