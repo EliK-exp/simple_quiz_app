199a504f2618526e9cad48fb0d48919f7114492a
Elyas Karimi <elyaskarimidarmiyan@gmail.com>
2022-03-16 09:23:21 +0330

    Added some admin customization to the app.

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db.models import Count
+
 from .models import Question, Category, Answer
 
 
@@ -6,11 +8,22 @@ from .models import Question, Category, Answer
 @admin.register(Question)
 class AdminQuestion(admin.ModelAdmin):
     list_display = ['question_text', 'category']
+    # I should provide a link to relative answers later
 
 
 @admin.register(Category)
 class AdminCategory(admin.ModelAdmin):
-    list_display = ['name']
+    list_display = ['name', 'number_of_category_questions']
+
+    @admin.display(ordering='category_questions_count')
+    def number_of_category_questions(self, categoty):
+        return categoty.question_set.all().count()
+
+    def get_queryset(self, request):
+        # print(super(AdminCategory, self).get_queryset(request).first().question_set.all())
+        qs = super().get_queryset(request)
+        return qs.annotate(
+            category_questions_count=Count('question'))  # I do not know why it is question and not questions-set!
 
 
 @admin.register(Answer)
